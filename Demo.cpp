674afe6c2d3b7cb945096e3c7431f241086c696b
AlmogShor <75311629+AlmogShor@users.noreply.github.com>
2023-05-15 12:33:46 +0300

    init tests

--- Demo.cpp
+++ Demo.cpp
@@ -20,7 +20,9 @@ using namespace ariel;
 int main() {
     Point a(32.3,44),b(1.3,3.5);
     assert(a.distance(b) == b.distance(a));
+
     Cowboy *tom = new Cowboy("Tom", a);
+
     OldNinja *sushi = new OldNinja("sushi", b);
     tom->shoot(sushi);
     cout << tom->print() <<endl;
@@ -28,12 +30,15 @@ int main() {
     sushi->move(tom);
     sushi->slash(tom);
 
-    Team team_A(tom); 
+
+    Team team_A(tom);
     team_A.add(new YoungNinja("Yogi", Point(64,57)));
+
 
     // Team b(tom); should throw tom is already in team a
 
      Team team_B(sushi);
+//     printf("team_B still alive: %d\n", team_B.stillAlive());
      team_B.add(new TrainedNinja("Hikari", Point(12,81)));
 
 
@@ -42,6 +47,7 @@ int main() {
         team_B.attack(&team_A);
         team_A.print();
         team_B.print();
+        printf("i");
      }
 
      if (team_A.stillAlive() > 0) cout << "winner is team_A" << endl;
---
+++ Test.cpp
@@ -0,0 +1,52 @@
+//
+// Created by shora on 15/05/2023.
+//
+
+#include "Team.hpp"
+#include <iostream>
+#include <sstream>
+#include "doctest.h"
+
+using namespace ariel;
+using namespace std;
+
+TEST_SUITE("point class methods"){
+    TEST_CASE("constructor"){
+        SUBCASE("default constructor"){
+            Point p1;
+            CHECK(p1.getX() == 0);
+            CHECK(p1.getY() == 0);
+        }
+        SUBCASE("constructor with parameters"){
+            Point p1(1,2);
+            CHECK(p1.getX() == 1);
+            CHECK(p1.getY() == 2);
+        }
+    }
+    TEST_CASE("distance"){
+        Point p1(1,2);
+        Point p2(3,4);
+        CHECK(p1.distance(p2) == sqrt(8));
+    }
+
+    TEST_CASE("print"){
+        Point p1(1,2);
+        ostringstream oss;
+        p1.print();
+        CHECK(oss.str() == "(1, 2)");
+    }
+
+    TEST_CASE("moveTowards"){
+        Point p1(1,2);
+        Point p2(3,4);
+        Point p3(2,3);
+        CHECK(p1.moveTowards(p1,p2,1).getX() == p3.getX());
+        CHECK(p1.moveTowards(p1,p2,1).getY() == p3.getY());
+    }
+
+    TEST_CASE("getters"){
+        Point p1(1,2);
+        CHECK(p1.getX() == 1);
+        CHECK(p1.getY() == 2);
+    }
+}
